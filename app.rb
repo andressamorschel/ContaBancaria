c8ca956dab1a5dcc2b256df7589da350156678b5
Andressa Morschel <andressamorschel@pop-os.localdomain>
2021-07-02 21:15:43 -0300

    corrigindo função de saque

--- app.rb
+++ app.rb
@@ -3,15 +3,19 @@ require_relative 'cliente.rb'
 class App 
 
   def menu 
-    puts "Selecione o que deseja fazer:\n1) Depositar\n2) Sacar"
+    puts "Selecione o que deseja fazer:\n1) Depositar\n2) Sacar\n3) Consultar saldo"
     resposta_cliente = gets.chomp.to_i
   end
 
   def operacao
     opcao_resposta = menu
+    cliente_operacao = Cliente.new("eu", "corrente", 50)
     if opcao_resposta == 1
-      cliente_operacao = Cliente.new("eu", "corrente", 580)
       cliente_operacao.depositar 
+    elsif opcao_resposta == 2  
+      cliente_operacao.sacar 
+    elsif opcao_resposta == 3  
+      cliente_operacao.consultar_saldo  
     end  
   end
 end
--- cliente.rb
+++ cliente.rb
@@ -5,19 +5,32 @@ class Cliente
     @saldo = saldo
   end
 
-  def pagar_conta(valor_pagamento)
-    valor_pagamento = gets.chomp.to_f
-    @saldo -= valor_pagamento
+  def consultar_saldo
+    puts "Saldo: #{@saldo}"
+  end  
+
+  def sacar
+    puts "Selecione o valor:"
+    valor_saque = gets.chomp.to_f
+    
+    if valor_saque <= @saldo
+      @saldo -= valor_saque
+      puts "Saque efetuado com sucesso!"
+    elsif  
+      puts "Ocorreu um erro inesperado"
+    end
   end
 
   def depositar
     puts "Selecione o valor:"
     valor_deposito = gets.chomp.to_f
-    if valor_deposito <= @saldo
+
+    if valor_deposito <= 10000
       @saldo += valor_deposito 
       puts "Depósito efetuado com sucesso"
     else
       puts "Ocorreu um erro inesperado"  
     end
+
   end  
 end
